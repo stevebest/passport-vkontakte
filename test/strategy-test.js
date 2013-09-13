5debc88d19ac6a9d7612e1613dcd71d3fe595c6d
Leonid Beschastny <lbeschastny@pliq.me>
2013-09-13 14:58:24 +0400

    Added some tests.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,6 +1,7 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
+var urlParser = require('url');
 var VKontakteStrategy = require('passport-vkontakte/strategy');
 
 
@@ -30,7 +31,16 @@ vows.describe('VKontakteStrategy').addBatch({
 
       // mock
       strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
-        var body = '{"response":[{"id":650715,"first_name":"Stepan","last_name":"Stolyarov","sex":2}]}';
+        var body = JSON.stringify({
+          response: [{
+            id: 650715,
+            first_name: "Stepan",
+            last_name: "Stolyarov",
+            sex: 2,
+            bdate: "1.2.1989",
+            photo_123: "my_photo"
+          }]
+        });
 
         callback(null, body, undefined);
       }
@@ -67,6 +77,46 @@ vows.describe('VKontakteStrategy').addBatch({
       'should set json property' : function(err, profile) {
         assert.isObject(profile._json);
       },
+      'should parse bdate field' : function(err, profile) {
+        assert.equal(profile.birthday, '1989-02-01');
+      },
+      'should parse photos' : function(err, profile) {
+        var photo = profile.photos[0];
+        assert.equal(photo.value, 'my_photo');
+        assert.equal(photo.type, 'photo_123');
+      },
+    },
+  },
+
+  'strategy when loading user profile with custom fields': {
+    topic: function() {
+      var self = this;
+
+      var strategy = new VKontakteStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        profileFields: ['custom_field_1', 'custom_field_2']
+      },
+      function() {});
+
+      // mock
+      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+        var query = urlParser.parse(url, true).query;
+        self.callback(null, query);
+      }
+
+      process.nextTick(function () {
+        strategy.userProfile('access-token', 650715, function() {});
+      });
+    },
+
+    'should request custom fields' : function(err, query) {
+      var foelds = query.fields.split(',');
+      assert.notEqual(foelds.indexOf('custom_field_1'), -1);
+      assert.notEqual(foelds.indexOf('custom_field_2'), -1);
+    },
+    'should have version flag in query' : function(err, query) {
+      assert.equal(query.v, '5.0');
     },
   },
 
